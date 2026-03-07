8390c16d4db51502a0ff53f796813f57159b708f
jhoar <johnhoar@yahoo.co.uk>
2026-03-07 17:03:29 +0100

    Add root Test task for --tests compatibility entrypoint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.testing.Test
+
 buildscript {
     repositories {
         google()
@@ -7,5 +9,20 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:8.5.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    }
+}
+
+plugins {
+    base
+}
+
+tasks.register<Test>("test") {
+    description = "Compatibility root test task for --tests filtering in CI/container."
+    group = LifecycleBasePlugin.VERIFICATION_GROUP
+
+    testClassesDirs = files()
+    classpath = files()
+    filter {
+        isFailOnNoMatchingTests = false
     }
 }
